38a845ec5dac596724d2794273b05d6b7546edc3
jdetok <jdeko17@gmail.com>
2026-03-02 14:01:03 -0600

    create etl pkg, branching

---
+++ pkg/etl/etl.go
@@ -0,0 +1,5 @@
+package etl
+
+// intent: provide standardized structs and interfaces with functionality for getting external data, transforming it
+// to a defined type, and loading into postgres/postgis.
+// attempting to replace individual workflows in gis package as of 3/2/2026
